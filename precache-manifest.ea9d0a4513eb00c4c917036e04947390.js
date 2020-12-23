8df4dd8e2792187724f9339c4275b1e63602fd4b
tgbf2674 <tgbf2020@163.com>
2020-12-23 15:33:41 +0800

    deploy

---
+++ precache-manifest.ea9d0a4513eb00c4c917036e04947390.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12dcbfcea00fc8acef97",
+    "url": "/morney-website/css/app.e4dd956d.css"
+  },
+  {
+    "revision": "9c03d1b1578c2539b794ac283d31fa42",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "12dcbfcea00fc8acef97",
+    "url": "/morney-website/js/app.df556254.js"
+  },
+  {
+    "revision": "feb640cf36d85d4cf81e",
+    "url": "/morney-website/js/chunk-vendors.6b781b09.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "d2798f435da7e7bebf116f51aa29bb89",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);

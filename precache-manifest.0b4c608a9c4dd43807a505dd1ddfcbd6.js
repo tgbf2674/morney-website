a9cd0bffafe94fc4fd8d6c5d06ef664eda116e1e
maqing <136518041@qq.com>
2021-10-08 14:26:58 +0800

    update

---
+++ precache-manifest.0b4c608a9c4dd43807a505dd1ddfcbd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294163a21611beebe10e",
+    "url": "/morney-website/css/app.d988bcf6.css"
+  },
+  {
+    "revision": "43e8e50149713d79b345",
+    "url": "/morney-website/css/chunk-vendors.56f50855.css"
+  },
+  {
+    "revision": "23638a6513d54da6200f3995dadc1266",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "294163a21611beebe10e",
+    "url": "/morney-website/js/app.6d741f99.js"
+  },
+  {
+    "revision": "43e8e50149713d79b345",
+    "url": "/morney-website/js/chunk-vendors.4b8d66c7.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  }
+]);
